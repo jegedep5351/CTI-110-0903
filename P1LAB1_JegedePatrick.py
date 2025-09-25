44a8a3ed3991f0aaa5a8c8d3416985d4a13ef97e
jegedep5351 <jegedep5351@student.faytechcc.edu>
2025-09-25 03:05:41 -0400

    Add files via upload

---
+++ P1LAB1_JegedePatrick.py
@@ -0,0 +1,8 @@
+# Name: Patrick O Jegede
+# Date: 2025-09-25
+# Assignment: P1LAB1
+# Description: This assignment starts with the introduction of Python language.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello " + last_name + "!, welcome to CTI-110")
